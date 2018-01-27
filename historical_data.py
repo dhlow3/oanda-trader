46eb1d6f689816354f60d5be81ed3b00386de449
dhlow3 <dh.low3@gmail.com>
2018-01-27 12:38:37 -0800

    Add argparse to historical_data.py

--- historical_data.py
+++ historical_data.py
@@ -1,12 +1,68 @@
-"""Get .csv data for feed."""
+"""Get .csv data file of historical FX prices."""
+import argparse
 import oandapyV20.endpoints.instruments as instruments
 import pandas as pd
 from oandapyV20 import API
+from os.path import exists
 
 from config import get_config
 
 
-def get_price_data(pair, config_file, dst, **kwargs):
+def get_args():
+    """Get arguments from command line.
+
+    Returns
+    ----------
+    args: dict
+        Arguments parsed from the command line and any defults not parsed.
+
+    """
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description='Get historical data for an FX pair.')
+    parser.add_argument('pair',
+                        help='currency pair for which data is required '
+                             '(e.g. EUR_USD)')
+    parser.add_argument('config_file',
+                        help='configuration file with Oanda access token')
+    parser.add_argument('-g',
+                        dest='granularity',
+                        metavar='GRANULARITY',
+                        default='M5',
+                        help='the granularity of the timeframe')
+    parser.add_argument('-c',
+                        dest='count',
+                        metavar='COUNT',
+                        default='30',
+                        type=int,
+                        help='the number of bars to get')
+    parser.add_argument('-o',
+                        dest='output',
+                        metavar='OUTPUT',
+                        default='./sample.csv',
+                        help='the path for the output file')
+    parser_args = parser.parse_args()
+
+    args = dict(pair=parser_args.pair.upper(),
+                config_file=parser_args.config_file,
+                granularity=parser_args.granularity,
+                count=parser_args.count,
+                output=parser_args.output)
+
+    # handle argument errors
+    assert '_' in args['pair'], "Currency pair must be '_' separated"
+    base, target = args['pair'].split('_')
+    assert len(base) == 3, 'base currency must have len of 3 e.g. EUR'
+    assert len(target) == 3, 'target currency must have len of 3 e.g. EUR'
+
+    assert exists(args['config_file'])
+
+    assert args['count'] > 0, 'count must be positive integer'
+
+    return args
+
+
+def get_price_data(pair, config_file, output, **kwargs):
     """Get data from Oanda and put in CSV.
 
     Parameters
@@ -15,15 +71,14 @@ def get_price_data(pair, config_file, dst, **kwargs):
         The instrument pair in which to fetch prices.
     config_file : str
         Location of configuration file.
-    dst: str
+    output: str
         Location and name of output .csv file.
 
     """
     conf = get_config(config_file)
-    params = dict(kwargs)
-    params['price'] = 'BA'
-    print(params)
-    r = instruments.InstrumentsCandles(instrument=pair, params=params)
+    kwargs['price'] = 'BA'
+
+    r = instruments.InstrumentsCandles(instrument=pair, params=kwargs)
     api = API(access_token=conf['token'])
     api.request(r)
 
@@ -37,14 +92,18 @@ def get_price_data(pair, config_file, dst, **kwargs):
 
     df = pd.DataFrame(prices)
     df.columns = ['time', 'bid', 'ask', 'spread']
-    df.to_csv(dst, sep='\t', index=False)
+    df.to_csv(output, sep='\t', index=False)
 
 
 if __name__ == '__main__':
-    params = {'granularity': 'M5', 'count': 10}
-    pair = 'EUR_USD'
+    # Get arguments from parser
+    args = get_args()
 
-    get_price_data(pair=pair,
-                   config_file='./oanda.conf',
-                   dst='./data/sample.csv',
-                   **params)
+    # Get required arguments for get_price_data
+    pair = args.pop('pair')
+    config_file = args.pop('config_file')
+    output = args.pop('output')
+
+    # Get price data
+    kwargs = args
+    get_price_data(pair, config_file, output, **kwargs)
